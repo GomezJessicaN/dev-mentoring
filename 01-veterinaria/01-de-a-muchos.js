e17620e20842ea48838d38bc5dd0709a9b2b4f6a
Guido Vilariño <gvilarino@gmail.com>
2018-08-13 19:41:42 -0300

    Add 01/01

---
+++ 01-veterinaria/01-de-a-muchos.js
@@ -0,0 +1,45 @@
+/*
+Carla quiere registrar actividades y evaluar el comportamiento de sus animales a
+través del programa que estamos escribiendo.
+
+EJERCICIO: escribir el código necesario para poder satisfacer las siguientes
+necesidades:
+
+A veces Carla alimenta a sus animales. Quiere poder registrarlo con nuestro
+programa. Ella les ofrece a todos la misma cantidad de comida, pero al mismo
+tiempo, con la misma cantidad de alimento.
+
+2.a: escribir una función que reciba unos animales y los haga comer a todos una
+cantidad de alimento determinada.
+
+Así como quiere poder registrar que sus animales comieron, quiere registrar que
+se desplazaron, ya sea corriendo o volando, la misma cantidad de metros.
+
+2.b: escribir una función que reciba unos animales y los haga moverse a todos
+una distancia determinada.
+
+Carla también quiere poder registrar cuando cualquiera de sus animales hace
+su gracia, ya sea ladrar, maullar o piar.
+
+2.c: escribir una función que reciba unos animales y los haga a todos hacer su
+gracia.
+
+*/
+
+animales = [ trooper, anubis, patan, pepita, pepona ]
+
+
+// 2.a
+function coman(unosAnimales, alimentoEnGramos) {
+  // escribir la solución acá
+}
+
+// 2.b
+function muevanse(unosAnimales, distancia) {
+  // escribir la solución acá
+}
+
+// 2.c
+function haganSuGracia(unosAnimales) {
+  //escribir la solución acá
+}
