c8dd657800d3648c7dc9edf79a67a839c3613750
Guido Vilariño <gvilarino@gmail.com>
2018-08-14 12:24:20 -0300

    Add 01/02

---
+++ 01-veterinaria/02-classes-and-instances.js
@@ -0,0 +1,13 @@
+/*
+Nuestro sistema ya funciona cumpliendo los requerimientos que nos pidió Carla.
+
+Pero nos vamos dando cuenta de que tiene problemas para escalar: introducir
+cambios que afecten a todos los animales de la misma especie nos implica copiar
+y pegar código, reproducir comportamiento en varios lugares. Y cuando haya que
+adaptar o cambiar esto entendemos que tendremos problemas.
+
+EJERCICIO: reescribir el código hasta ahora realizado utilizando clases de
+de Javascript para representar a los gatos, perros y cotorras.
+
+*/
+
