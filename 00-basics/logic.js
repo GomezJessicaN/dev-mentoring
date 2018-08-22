b65bd8f0f32bbee1279fb0e158020642a8a2f17c
Guido Vilariño <gvilarino@gmail.com>
2018-08-22 15:15:41 -0300

    Add 00 and first logic exercise

---
+++ 00-basics/logic.js
@@ -0,0 +1,7 @@
+/*
+Escribir la función `esBisiesto`, que recibe un número y retorna si dicho número
+es o no bisiesto.
+
+Un año es bisiesto si es múltiplo de 4. Esto no es válido para los años
+múltiplos de 100, excepto los múltiplos de 400.
+ */
